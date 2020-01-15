93809082b581fe18811383ba3f48b8238c986b59
fede-longhi-cg <federico.longhi@cloudgaia.com>
2020-01-15 16:05:50 -0300

    change main again

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 import (
 	"net/http"
 	"log"
-	"fmt"
-	"html"
 	"os"
 )
 
@@ -13,10 +11,6 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request){
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(`{"message": "hello world"}`))
-}
-
-func handlerFunction (w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
 
 func main(){
