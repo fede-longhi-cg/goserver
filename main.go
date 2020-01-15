1c1d5cf7d863bdbff7f0df67c1e5583ab23fcb5d
fede-longhi-cg <federico.longhi@cloudgaia.com>
2020-01-15 16:03:56 -0300

    change main

--- main.go
+++ main.go
@@ -7,6 +7,14 @@ import (
 	"os"
 )
 
+type server struct{}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"message": "hello world"}`))
+}
+
 func handlerFunction (w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
@@ -17,7 +25,8 @@ func main(){
 		log.Fatal("$PORT must be set")
 	}
 
-	http.HandleFunc("/", handlerFunction)
+	s = &server{}
+	http.Handle("/", s)
 
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
