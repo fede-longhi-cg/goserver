104964144e6891c6bdc72bb19944093966a79b6a
fede-longhi-cg <federico.longhi@cloudgaia.com>
2020-05-13 09:33:09 -0300

    all functions with loop

--- main.go
+++ main.go
@@ -9,34 +9,34 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// func homeLink(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "application/json")
-// 	switch r.Method {
-// 	case "GET":
-// 		w.WriteHeader(http.StatusOK)
-// 		w.Write([]byte(`{"message": "get called"}`))
-// 	case "POST":
-// 		w.WriteHeader(http.StatusCreated)
-// 		w.Write([]byte(`{"message": "post called"}`))
-// 	case "PUT":
-// 		w.WriteHeader(http.StatusAccepted)
-// 		w.Write([]byte(`{"message": "put called"}`))
-// 	case "DELETE":
-// 		w.WriteHeader(http.StatusOK)
-// 		w.Write([]byte(`{"message": "delete called"}`))
-// 	default:
-// 		w.WriteHeader(http.StatusNotFound)
-// 		w.Write([]byte(`{"message": "not found"}`))
-// 	}
-// }
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "get called"}`))
+	case "POST":
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"message": "post called"}`))
+	case "PUT":
+		w.WriteHeader(http.StatusAccepted)
+		w.Write([]byte(`{"message": "put called"}`))
+	case "DELETE":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "delete called"}`))
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"message": "not found"}`))
+	}
+}
 
-// func regs(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "application/json")
-// 	if r.Method == "GET" {
-// 		w.WriteHeader(http.StatusOK)
-// 		w.Write([]byte(`{"message": ["1","2","3"]}`))
-// 	}
-// }
+func regs(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method == "GET" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": ["1","2","3"]}`))
+	}
+}
 
 func loopHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -66,8 +66,8 @@ func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 
-	//router.HandleFunc("/", homeLink)
-	//router.HandleFunc("/regs-01", regs)
+	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/regs-01", regs)
 	router.HandleFunc("/loop/{number}", loopHandler)
 
 	log.Fatal(http.ListenAndServe(":"+port, router))
